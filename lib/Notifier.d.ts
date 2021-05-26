d23b4770e0047e74a487e488dd3d11283b58343a
igor <igor.kolosov@audiocodes.com>
2021-05-26 13:01:53 +0300

    improved ts definitions

--- lib/Notifier.d.ts
+++ lib/Notifier.d.ts
@@ -2,9 +2,22 @@ import {EventEmitter} from 'events'
 import {SUBSCRIBE, NOTIFY} from './Constants'
 import * as Utils from './Utils'
 
+declare enum NotifierTerminatedCode {
+    NOTIFY_RESPONSE_TIMEOUT = 0, 
+    NOTIFY_TRANSPORT_ERROR = 1, 
+    NOTIFY_NON_OK_RESPONSE = 2, 
+    NOTIFY_FAILED_AUTHENTICATION = 3,
+    SEND_FINAL_NOTIFY = 4, 
+    RECEIVE_UNSUBSCRIBE = 5, 
+    SUBSCRIPTION_EXPIRED = 6  
+}
+
 export class Notifier extends EventEmitter {
   setActiveState(): void;
   sendNotify(body?: string): void; 
   sendFinalNotify(body?: string, reason?: string): void; 
   get state(): string;
+  get id(): string;
+  static get C(): typeof NotifierTerminatedCode;
+  get C(): typeof NotifierTerminatedCode;
 }

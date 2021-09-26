1e565a451009b8b9f85009d68e8bbb845284154f
igor <igor.kolosov@audiocodes.com>
2021-09-26 12:01:40 +0300

    unit test improvement: eventSequence checking

--- test/test-UA-subscriber-notifier.js
+++ test/test-UA-subscriber-notifier.js
@@ -7,17 +7,15 @@ const LoopSocket = require('./include/LoopSocket');
 module.exports = {
   'subscriber/notifier communication' : function(test)
   {
-    test.expect(17);
-	
-    let notifyCounter = 0;
-    const notifyMaxCounter = 1;
+    test.expect(26);
+    
+    let eventSequence = 0;
     const weatherRequest = 'Please report the weather condition';
     const weatherReport = '+20..+24°C, no precipitation, light wind';
     const contentType = 'text/plain'; 
     
     function createNotifier(ua, subscribe) 
     {
-  
       const notifier = ua.notify(subscribe, contentType, { pending: false });
 
       // Receive subscribe (includes initial)
@@ -28,16 +26,23 @@ module.exports = {
  
         if (isUnsubscribe)
         {
+          test.ok(++eventSequence === 9, 'receive un-subscribe, send final notify');   
+          
           notifier.terminate(weatherReport);
         }
         else 
         {
+          test.ok(++eventSequence === 4, 'receive subscribe, send notify'); 
+          
           notifier.notify(weatherReport);
         }
       });
 
       notifier.on('terminated', (terminationCode, sendFinalNotify) => 
       {
+        test.ok(++eventSequence === 10, 'notifier terminated');       
+        test.ok(!sendFinalNotify, 'here final notify sending if subscription expired');
+        
         if (sendFinalNotify) 
         {
           notifier.terminate(weatherReport);
@@ -62,36 +67,44 @@ module.exports = {
 
       subscriber.on('active', () => 
       {
-        test.ok(true, 'receive NOTIFY with Subscription-state: active');
+        test.ok(++eventSequence === 6, 'receive notify with subscription-state: active');  
       });
 
       subscriber.on('notify', (isFinal, notify, body, contType) => 
       {
+        eventSequence++;
+        test.ok(eventSequence === 7 || eventSequence === 11, 'receive notify');
         test.strictEqual(body, weatherReport, 'received notify body');
         test.strictEqual(contType, contentType, 'received notify content-type');
-		
-        if (++notifyCounter === notifyMaxCounter)
+        
+        // After receiving the first notify, send un-subscribe.
+        if (eventSequence === 7)
         { 
-          test.ok(true, 'send unsubscrige');
+          test.ok(++eventSequence === 8, 'send un-subscribe');
+          
           subscriber.terminate(weatherRequest);
         }
       });
 
       subscriber.on('terminated', (terminationCode, reason, retryAfter) => 
       {
+        test.ok(++eventSequence === 12, 'subscriber terminated');
         test.ok(terminationCode === subscriber.C.RECEIVE_FINAL_NOTIFY);   
         test.ok(reason === undefined);
-        test.ok(retryAfter === undefined);		
-		
+        test.ok(retryAfter === undefined);      
+        
         ua.stop();
+        
         test.done();    
       });
 
       subscriber.on('dialogCreated', () => 
       {
-        test.ok(true, 'subscribe dialog created');
+        test.ok(++eventSequence === 5, 'subscriber - dialog created');
       });
 
+      test.ok(++eventSequence === 2, 'send subscribe');
+      
       subscriber.subscribe(weatherRequest);
     }
 
@@ -104,18 +117,19 @@ module.exports = {
     };
 
     const ua = new JsSIP.UA(config);
-	
+    
     // Uncomment to see SIP communication
     // JsSIP.debug.enable('JsSIP:*');
     
     ua.on('newSubscribe', (e) => 
     {
-      test.ok(true, 'receive SUBSCRIBE');
+      test.ok(++eventSequence === 3, 'receive initial subscribe');    
 
       const subs = e.request;
       const ev = subs.parseHeader('event');
       
       test.strictEqual(ev.event, 'weather');
+      
       if (ev.event !== 'weather')
       {
         subs.reply(489); // "Bad Event"
@@ -127,6 +141,7 @@ module.exports = {
       const isAcceptOK = accepts && accepts.some((v) => v.includes('text/plain'));
       
       test.ok(isAcceptOK, 'notifier understand subscribe accept header');
+      
       if (!isAcceptOK)
       {
         subs.reply(406); // "Not Acceptable"
@@ -139,6 +154,8 @@ module.exports = {
         
     ua.on('connected', () => 
     {
+      test.ok(++eventSequence === 1, 'socket connected event');    
+      
       createSubscriber(ua);
     });
         
